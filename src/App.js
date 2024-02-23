f7e52210f262990d26e40a7b34aa6efb7bcea75a
Muhammad Ghazanfar <116085344+blackhorsese@users.noreply.github.com>
2024-02-24 00:08:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import { Routes, Route } from "react-router-dom"
 import Index from './Pages/Home/Index';
 import Login from "./Pages/Login";
+import React, { useEffect, useState } from 'react';
+import Preloader from './Pages/Preloader';
 
 function App() {
-  return (
-      <Routes>
-        <Route path="/" element={ <Index/> } />
-        <Route path="/login" element={ <Login/> } />
+  const [loading, setLoading] = useState(true);
 
-      </Routes>
+  useEffect(() => {
+    // Simulate a delay to mimic content loading
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000); // Adjust the delay as needed
+
+    // You can replace the above setTimeout with your actual data fetching logic.
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <Routes>
+          <Route path="/" element={ <Index/> } />
+          <Route path="/login" element={ <Login/> } />
+
+        </Routes>
+      )}
+    </>
+
   )
 }
 
