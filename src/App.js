5eec266017b9c779995e5ea7874abf6e4bea7ab8
Muhammad Ghazanfar <116085344+blackhorsese@users.noreply.github.com>
2024-03-16 06:52:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { Routes, Route } from "react-router-dom"
 import Index from './Pages/Home/Index';
 import Login from "./Pages/Login";
 import React, { useEffect, useState } from 'react';
+import EV from './Pages/EV Charging/Index';
 import Preloader from './Pages/Preloader';
+import Cybersecurity from './Pages/Cyber Security/index';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -23,6 +25,8 @@ function App() {
       ) : (
         <Routes>
           <Route path="/" element={ <Index/> } />
+          <Route path="/electric-vehicle" element={ <EV/> } />
+          <Route path="/cyber-secruity" element={ <Cybersecurity/> } />
           <Route path="/login" element={ <Login/> } />
 
         </Routes>
